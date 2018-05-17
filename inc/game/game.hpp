9224aeba32954463704482faac29b592d711d7e3
Clemon <r.goulmot@hotmail.fr>
2018-05-18 01:44:01 +0200

    base of the structured code

---
+++ inc/game/game.hpp
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2018
+** indie studio
+** File description:
+** game
+*/
+
+#ifndef GAME_HPP_
+	#define GAME_HPP_
+
+	#include "irrlicht/irrlicht.h"
+	#include <iostream>
+	#include <memory>
+
+class	game
+{
+public:
+	game();
+	~game();
+};
+#endif /* !GAME_HPP_ */
---
+++ src/game/game.cpp
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2018
+** indie studio
+** File description:
+** game
+*/
+
+#include "game/game.hpp"
+
+game::game()
+{
+	std::cout << "game: init...\n";
+	std::cout << "game: initiated\n";
+}
+
+game::~game()
+{
+	std::cout << "game: destructing...\n";
+	std::cout << "game: destructed\n";
+}
